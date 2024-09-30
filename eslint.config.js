17ca84fa6d810e668ef490618f182b801ee21f46
Imre <imre.boersma@gmail.com>
2024-09-30 14:34:16 +0200

    chore: :wrench: add workspace config and ESLint edits

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,18 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  {files: ["./src/*.ts"]},
-  {languageOptions: { globals: [globals.browser, globals.builtin] }},
+  { files: ["./src/*.ts"] },
+  {
+    languageOptions: {
+      globals: [globals.browser, globals.builtin]
+    },
+
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-expressions": "warn"
+    }
+  }
 ];
