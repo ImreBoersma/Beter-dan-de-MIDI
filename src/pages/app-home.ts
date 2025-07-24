2b19b8e1e81029d050894d6b283d6bdcdd2e8ab2
Imre Boersma <imre@feestfabriek.nl>
2025-07-24 15:09:57 +0200

    fix: update confetti import path in app-home.ts

--- src/pages/app-home.ts
+++ src/pages/app-home.ts
@@ -2,7 +2,7 @@ import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { TemplateResult } from 'lit-html';
 
-import '../confetti/index.min.js';
+import '/confetti.min.js';
 import '../components/custom-audio-player.js';
 
 type Audio = {
