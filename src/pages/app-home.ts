f07e0d1954974be384566b926bc1d23d49841c29
Imre Boersma <imre.boersma@gmail.com>
2026-05-26 07:24:40 +0000

    fix: update import statement for TemplateResult to use type

--- src/pages/app-home.ts
+++ src/pages/app-home.ts
@@ -1,6 +1,6 @@
 import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { TemplateResult } from 'lit-html';
+import type { TemplateResult } from 'lit-html';
 
 import '../components/custom-audio-player.js';
 
